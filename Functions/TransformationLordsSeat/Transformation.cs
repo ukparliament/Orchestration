e67f3fff7cbc6e87c68599b99bd35f3b0b556329
hellikopter <hellikopter@users.noreply.github.com>
2017-11-28 10:36:20 +0000

    refactored houseseattype retrieval

--- Functions/TransformationLordsSeat/Transformation.cs
+++ Functions/TransformationLordsSeat/Transformation.cs
@@ -10,11 +10,12 @@ namespace Functions.TransformationLordsSeat
 {
     public class Transformation : BaseTransformation<Settings>
     {
+        private readonly string idNamespace = Environment.GetEnvironmentVariable("IdNamespace", EnvironmentVariableTarget.Process);
+
         public override IOntologyInstance[] TransformSource(string response)
         {
             IHouseSeat houseSeat = new HouseSeat();
             JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response);
-            string idNamespace = Environment.GetEnvironmentVariable("IdNamespace", EnvironmentVariableTarget.Process);
 
             string id = ((JValue)jsonResponse.SelectToken("ID0")).GetText();
             Uri uri = null;
@@ -35,25 +36,15 @@ namespace Functions.TransformationLordsSeat
             }
             houseSeat.HouseSeatName = ((JValue)jsonResponse.SelectToken("Name")).GetText();
 
-            object houseSeatTypeId = ((JValue)jsonResponse.SelectToken("Type_x0020_ID_x0020__x0028_for_x.Value"))?.Value;
-            if (houseSeatTypeId == null)
-            {
-                logger.Warning("No house seat type Id info found");
+            Uri houseSeatTypeUri = giveMeUri(jsonResponse, "Type_x0020_ID_x0020__x0028_for_x");
+            if (houseSeatTypeUri == null)
                 return null;
-            }
             else
-            {
-                if (Uri.TryCreate($"{idNamespace}{id}", UriKind.Absolute, out Uri houseSeatTypeUri))
-                    houseSeat.HouseSeatHasHouseSeatType = new HouseSeatType()
-                    {
-                        SubjectUri = houseSeatTypeUri
-                    };
-                else
+                houseSeat.HouseSeatHasHouseSeatType = new HouseSeatType()
                 {
-                    logger.Warning($"Invalid url '{id}' found");
-                    return null;
-                }
-            }
+                    SubjectUri = houseSeatTypeUri
+                };
+
 
             Uri houseOfLordsUri = IdRetrieval.GetSubject("houseName", "House of Lords", false, logger);
             houseSeat.HouseSeatHasHouse = new House()
@@ -80,6 +71,26 @@ namespace Functions.TransformationLordsSeat
             IHouseSeat houseSeatTypes = source.OfType<IHouseSeat>().SingleOrDefault();
 
             return new IOntologyInstance[] { houseSeatTypes };
+        }
+
+        private Uri giveMeUri(JObject jsonResponse, string tokenName)
+        {
+            object id = ((JValue)jsonResponse.SelectToken($"{tokenName}.Value"))?.Value;
+            if (id == null)
+            {
+                logger.Warning($"No {tokenName} Id info found");
+                return null;
+            }
+            else
+            {
+                if (Uri.TryCreate($"{idNamespace}{id}", UriKind.Absolute, out Uri uri))
+                    return uri;
+                else
+                {
+                    logger.Warning($"Invalid url '{id}' found");
+                    return null;
+                }
+            }
         }
     }
 }

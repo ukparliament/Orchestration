c13817359b89236e2a2a96289058b972f07247fd
hellikopter <hellikopter@users.noreply.github.com>
2018-02-21 12:41:01 +0000

    initial changes to automate deployment of logic apps based on source type
    Related Work Items: #510

--- LogicApps/Settings.ps1
+++ LogicApps/Settings.ps1
@@ -34,13 +34,23 @@ $apiProductOrchestration=Get-AzureRmApiManagementProduct -Context $management -T
 $subscription=Get-AzureRmApiManagementSubscription -Context $management -ProductId $apiProductOrchestration.ProductId
 $subscriptionKey=$subscription.PrimaryKey
 
+Enum SourceType
+{
+	Mnis = 1
+	Sharepoint = 2
+	External = 3
+	GovernmentRegister = 4
+	Custom = 5
+}
+
 $logicAppVariable=@(
     New-Object  -TypeName PSObject -Property @{
         "name"="constituencymnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/Constituencies?`$select=Constituency_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="17:00";
@@ -50,6 +60,7 @@ $logicAppVariable=@(
     }     
     New-Object  -TypeName PSObject -Property @{
         "name"="constituencyos";
+		"sourceKind"=[SourceType]::External;
         "listUri"="http://data.ordnancesurvey.co.uk/datasets/os-linked-data/apis/sparql?query=construct+%7B%3Fs+a+%3Chttp%3A%2F%2Fdata.ordnancesurvey.co.uk%2Fontology%2Fadmingeo%2FWestminsterConstituency%3E.%7D+WHERE+%7B%3Fs+a+%3Chttp%3A%2F%2Fdata.ordnancesurvey.co.uk%2Fontology%2Fadmingeo%2FWestminsterConstituency%3E.%7D";
         "listAcceptHeader"="application/rdf+xml";
         "foreachObject"="@json(body('Get_List'))['rdf:RDF']['j.0:WestminsterConstituency']";
@@ -63,6 +74,7 @@ $logicAppVariable=@(
     }
     New-Object  -TypeName PSObject -Property @{
         "name"="constituencyosni";
+		"sourceKind"=[SourceType]::External;
         "listUri"="https://gisservices.spatialni.gov.uk/arcgisc/rest/services/OpenData/OSNIOpenData_50KBoundaries/MapServer/3/query?returnIdsOnly=true&where=1%3D1&f=JSON";
         "listAcceptHeader"="text/plain";
         "foreachObject"="@json(body('Get_List')).objectIds";
@@ -76,11 +88,12 @@ $logicAppVariable=@(
     }
     New-Object  -TypeName PSObject -Property @{
         "name"="contactpointseatmnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/MembersDataPlatform/open/OData.svc/MemberAddresses?`$select=MemberAddress_Id&`$filter=AddressType_Id%20eq%201%20or%20AddressType_Id%20eq%203";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
-        "frequency"="hour";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
+		"frequency"="hour";
         "interval"=24;
         "triggerTime"="18:50";
         "queueReadBatchSize"=150;
@@ -89,10 +102,11 @@ $logicAppVariable=@(
     }
     New-Object  -TypeName PSObject -Property @{
         "name"="contactpointelectoralmnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/MembersDataPlatform/open/OData.svc/MemberAddresses?`$select=MemberAddress_Id&`$filter=AddressType_Id%20eq%204";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="19:15";
@@ -102,10 +116,11 @@ $logicAppVariable=@(
     }
     New-Object  -TypeName PSObject -Property @{
         "name"="contactpointpersonmnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/MembersDataPlatform/open/OData.svc/MemberAddresses?`$select=MemberAddress_Id&`$filter=AddressType_Id%20eq%205";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="19:45";
@@ -115,10 +130,11 @@ $logicAppVariable=@(
     }
     New-Object -TypeName PSObject -Property @{
         "name"="partymnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/MembersDataPlatform/open/OData.svc/Parties?`$select=Party_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="19:50";
@@ -128,10 +144,11 @@ $logicAppVariable=@(
     }
     New-Object -TypeName PSObject -Property @{
         "name"="membermnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/Members?`$select=Member_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="20:20";
@@ -141,10 +158,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="governmentincumbencymnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/MemberGovernmentPosts?`$select=MemberGovernmentPost_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="17:30";
@@ -154,10 +172,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="oppositionincumbencymnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/MemberOppositionPosts?`$select=MemberOppositionPost_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="23:55";
@@ -167,10 +186,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="partymembershipmnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/MemberParties?`$select=MemberParty_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="18:00";
@@ -180,10 +200,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="seatincumbencymnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/MemberConstituencies?`$select=MemberConstituency_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="18:15";
@@ -193,10 +214,11 @@ $logicAppVariable=@(
     }
     New-Object -TypeName PSObject -Property @{
         "name"="epetition";
+		"sourceKind"=[SourceType]::Custom;
         "listUri"="";
         "listAcceptHeader"="";
-        "foreachObject"="@json('')";
-        "idObject"="@json('')";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="22:10";
@@ -206,6 +228,7 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="territory";
+		"sourceKind"=[SourceType]::GovernmentRegister;
         "listUri"="";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
@@ -219,6 +242,7 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="country";
+		"sourceKind"=[SourceType]::GovernmentRegister;
         "listUri"="";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
@@ -232,10 +256,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="committeemnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/Committees?`$select=Committee_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="21:05";
@@ -245,10 +270,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="membercommitteemnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/MemberCommittees?`$select=MemberCommittee_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="21:25";
@@ -258,7 +284,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="weblink";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="7cb6d78b-5f45-49d2-bb77-009f71a83390";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -271,7 +298,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="photo";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="2460c6bf-f26e-4049-94d0-c6096e036f3a";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -284,10 +312,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="governmentpostmnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/GovernmentPosts?`$select=GovernmentPost_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="20:00";
@@ -297,10 +326,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="oppositionpostmnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/membersdataplatform/open/OData.svc/OppositionPosts?`$select=OppositionPost_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="20:15";
@@ -310,7 +340,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="houseseattype";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="85fe54cd-d26a-43af-88a5-9063f747c4a9";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -323,7 +354,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="lordsseat";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="4a000e4a-aa78-4338-806d-92c0dd42e1a6";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -336,7 +368,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="lordsseatincumbency";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="a9c47d77-9ccb-44cc-bc90-c3d07ea8aa0e";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -349,10 +382,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="departmentmnis";
+		"sourceKind"=[SourceType]::Custom;
         "listUri"="";
         "listAcceptHeader"="";
-        "foreachObject"="@json('')";
-        "idObject"="@json('')";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="20:01";
@@ -362,7 +396,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="departmentgovernmentorganisation";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="a55100f4-8dd9-43a3-8614-3a3f121c8fbf";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -375,10 +410,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="committeechairincumbencymnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/MembersDataPlatform/open/OData.svc/MemberCommitteeChairs?`$select=MemberCommitteeChair_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="20:40";
@@ -388,10 +424,11 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="committeelaymembermnis";
+		"sourceKind"=[SourceType]::Mnis;
         "listUri"="http://data.parliament.uk/MembersDataPlatform/open/OData.svc/CommitteeLayMembers?`$select=CommitteeLayMember_Id";
-        "listAcceptHeader"="application/atom+xml";
-        "foreachObject"="@json(body('Get_List')).feed.entry";
-        "idObject"="@{item().id}";
+        "listAcceptHeader"="";
+        "foreachObject"="";
+        "idObject"="";
         "frequency"="hour";
         "interval"=24;
         "triggerTime"="21:03";
@@ -401,7 +438,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="contactpointhouse";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="261f09cd-9c4e-4e76-a928-b7b8c5b3cdcd";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -414,7 +452,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="committee";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="035fa54f-492a-4008-93da-5ee912a2d0cf";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -427,7 +466,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="committeetype";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="774e8a14-a613-4948-a645-a434ef6a8f1f";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -440,7 +480,8 @@ $logicAppVariable=@(
     }
 	New-Object -TypeName PSObject -Property @{
         "name"="parliamentperiod";
-        "listUri"="";
+		"sourceKind"=[SourceType]::Sharepoint;
+        "listUri"="ffcfa882-ee6d-4467-b146-68cd62924256";
         "listAcceptHeader"="";
         "foreachObject"="@json('')";
         "idObject"="@json('')";
@@ -453,15 +494,25 @@ $logicAppVariable=@(
     }
 )
 
-$variableNames=@("name","listUri","listAcceptHeader","foreachObject","idObject","frequency","interval","triggerTime","queueReadBatchSize","queueReadInterval","queueReadFrequency")
-
 Log "Setting variables to use during deployment"
-Log "Number of settings: $($logicAppVariable.Length)"
-foreach ($key in $variableNames) {
-    Log "Key: $key"
-    $arr=($logicAppVariable | Select-Object $key).$key
-    $json= ConvertTo-Json $arr -Compress
-    Write-Host "##vso[task.setvariable variable=LogicAppsSetting_$key]$json"
+foreach ($kind in [enum]::GetValues([SourceType])) {
+	switch ($kind) {
+        "$([SourceType]::Custom)" {
+            break
+        }
+        "$([SourceType]::External)" {            
+            $settings=($logicAppVariable | Where-Object sourceKind -EQ $kind | Select-Object name, listUri, listAcceptHeader, foreachObject, idObject)
+            $json=ConvertTo-Json @{settings=$settings} -Compress
+            Write-Host "##vso[task.setvariable variable=LogicAppsSetting_$kind]$json"
+            break
+        }
+        default {
+            $settings=($logicAppVariable | Where-Object sourceKind -EQ $kind | Select-Object name, listUri)
+			$json=ConvertTo-Json @{settings=$settings} -Compress
+            Write-Host "##vso[task.setvariable variable=LogicAppsSetting_$kind]$json"
+            break
+        }
+    }    
 }
 Write-Host "##vso[task.setvariable variable=SubscriptionKeyOrchestration]$subscriptionKey"
 

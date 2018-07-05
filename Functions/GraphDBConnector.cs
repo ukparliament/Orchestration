456455aa53979024407c66df6278c82c2c8f0fb3
hellikopter <hellikopter@users.noreply.github.com>
2018-07-05 12:42:37 +0100

    provenance - initial version
    Related Work Items: #627

--- Functions/GraphDBConnector.cs
+++ Functions/GraphDBConnector.cs
@@ -42,8 +42,9 @@ namespace Functions
                 sb.AppendLine("INSERT DATA {");
                 foreach (Triple added in additions)
                     sb.AppendLine(sparqlFormatter.Format(added));
-                sb.AppendLine("}");                
+                sb.AppendLine("};");
             }
+            sb.AppendLine(addProvenance(additions, removals));
             string sparqlUpdate = sb.ToString();
             SparqlUpdateParser parser = new SparqlUpdateParser();
             parser.ParseFromString(sparqlUpdate);
@@ -58,6 +59,42 @@ namespace Functions
             }
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             response.Dispose();
+        }
+
+        private static string addProvenance(IEnumerable<Triple> additions, IEnumerable<Triple> removals)
+        {
+            Graph graph = new Graph();
+            StringBuilder sb = new StringBuilder();
+            SparqlFormatter sparqlFormatter = new SparqlFormatter();
+            DateTimeOffset dt = DateTimeOffset.UtcNow;
+            sb.AppendLine("INSERT DATA {");
+            foreach (Triple removed in removals)
+            {
+                sb.Append($@"[] <http://example.com/subject> {sparqlFormatter.Format(removed.Subject)};
+                        <http://example.com/predicate> {sparqlFormatter.Format(removed.Predicate)};
+                        <http://example.com/dateTime> {sparqlFormatter.Format(dt.ToLiteral(graph))};
+                        <http://example.com/oldObject> {sparqlFormatter.Format(removed.Object)}");
+                IEnumerable<Triple> foundAdded = additions.Where(a => a.HasSubject(removed.Subject) && a.HasPredicate(removed.Predicate));
+                if (foundAdded.Any())
+                {
+                    sb.AppendLine(";");
+                    foreach (Triple added in foundAdded)
+                        sb.AppendLine($@"<http://example.com/newObject> {sparqlFormatter.Format(added.Object)}.");
+                }
+                else
+                    sb.AppendLine($@".");
+            }
+            foreach (Triple added in additions)
+            {
+                if (removals.Any(a => a.HasSubject(added.Subject) && a.HasPredicate(added.Predicate)) == false)
+                    sb.Append($@"[] <http://example.com/subject> {sparqlFormatter.Format(added.Subject)};
+                        <http://example.com/predicate> {sparqlFormatter.Format(added.Predicate)};
+                        <http://example.com/dateTime> {sparqlFormatter.Format(dt.ToLiteral(graph))};
+                        <http://example.com/newObject> {sparqlFormatter.Format(added.Object)}.");
+            }
+            sb.AppendLine("}");
+
+            return sb.ToString();
         }
 
         protected override HttpWebRequest CreateRequest(String servicePath, String accept, String method, Dictionary<String, String> queryParams)
